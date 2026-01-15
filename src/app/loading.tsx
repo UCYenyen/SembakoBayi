d906b00e5af8c0ab43b11492fdab5f07a5880069
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-16 02:31:47 +0700

    upgrade loading view

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Metadata } from 'next'
 import { FaSpinner } from 'react-icons/fa6'
-import { Card } from '@/components/ui/shadcn-ui/card'
+import { Card, CardContent, CardDescription, CardFooter } from '@/components/ui/shadcn-ui/card'
+import { CardHeader, CardTitle } from '@/components/ui/shadcn-ui/card'
+import { Separator } from '@/components/ui/shadcn-ui/separator'
+import Link from 'next/link'
+import { TriangleAlert } from 'lucide-react'
 
 export const metadata: Metadata = {
   title: "Memuat...",
@@ -9,12 +13,23 @@ export const metadata: Metadata = {
 }
 export default function loading() {
   return (
-    <main className="overflow-hidden py-32 pt-48 justify-center items-center flex flex-col gap-4">
-      <Card className="w-112.5 max-w-[80%] shadow-lg flex justify-center items-center flex-col gap-6 p-8">
-        <FaSpinner className="animate-spin text-4xl" />
-        <h1 className="text-4xl font-bold text-center">Memuat...</h1>
-        <p className="text-center mt-4">Harap tunggu sementara konten dimuat.</p>
-      </Card>
-    </main>
+    <section className='py-32 pt-48 w-screen overflow-hidden flex justify-center items-center'>
+            <Card className='w-112.5 max-w-[80%] min-h-72 flex flex-col justify-between shadow-lg'>
+                <CardHeader>
+                    <div className='flex justify-center items-center gap-4'>
+                        <CardTitle className='text-2xl'>Memuat</CardTitle>
+                    </div>
+                </CardHeader>
+                <Separator />
+                <CardContent>
+                    <CardDescription className='text-center flex flex-col gap-4 items-center justify-center'>
+                        <div className='p-4 flex justify-center items-center bg-gray-200 rounded-full'>
+                           <FaSpinner className="animate-spin text-4xl" />
+                        </div>
+                        <p className='my-4 text-center w-[90%]'>Harap tunggu sementara konten dimuat.</p>
+                    </CardDescription>
+                </CardContent>
+            </Card>
+        </section>
   )
 }
