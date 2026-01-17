22987d149f53cfed7d839c32c60f04464d73b098
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-17 23:18:31 +0700

    get all product

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,25 +1,17 @@
 import ShopSection from '@/components/ui/personal/pages/shop/ShopSection'
 import { Metadata } from 'next'
-
+import { getAllProduct } from '@/lib/services/product'
 export const metadata: Metadata = {
   title: 'Belanja - SembakoBayi',
   description: 'Beli kebutuhan bayi Anda dengan mudah di SembakoBayi. Temukan berbagai produk berkualitas untuk si kecil. Belanja sekarang dan nikmati kemudahan berbelanja online!',
 }
 
-export default function ShopPage() {
-  const dummyProducts = Array.from({ length: 12 }).map((_, i) => ({
-    id: crypto.randomUUID(),
-    name: `Produk Bayi Premium ${i + 1}`,
-    price: (i + 1) * 50000 + 100000,
-    rating: Math.floor(Math.random() * 2) + 4,
-    imageSrc: `/images/products/Apel50.webp`,
-    isOnSale: false,
-    discountAmount: 25000,
-  }))
+export default async function ShopPage() {
+  const products = await getAllProduct(1);
 
   return (
     <main className="min-h-screen w-full bg-background py-12">
-      <ShopSection Products={dummyProducts} />
+      <ShopSection Products={products} />
     </main>
   )
 }
--- src/lib/services/product.ts
+++ src/lib/services/product.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/utils/prisma";
+import { ProductCardProps } from "@/types/product.md";
 
 export async function getAllFeaturedProducts() {
   return prisma.product.findMany({
@@ -12,6 +13,44 @@ export async function getAllFeaturedProducts() {
         }
     }
   });
+}
+
+export async function getAllProduct(page: number = 1) : Promise<ProductCardProps[]> {
+  const pageSize = 8;
+  const skip = (page - 1) * pageSize;
+  
+  const products = await prisma.product.findMany({
+    include: {
+      brand: true,
+      category: true,
+    },
+    where:{
+        NOT:{
+            isHidden:true
+        }
+    },
+    skip,
+    take: pageSize,
+  });
+
+  return products.map((product) => ({
+    id: product.id,
+    name: product.name,
+    price: Number(product.price),
+    rating: 5, 
+    imageSrc: product.imageUrl,
+    isOnSale: product.onSale,
+    discountAmount: Number(product.promoPrice || 0),
+    category: {
+      id: product.category.id,
+      name: product.category.name,
+      parent_id: product.category.parentId || "",
+    },
+    brand: {
+      id: product.brand.id,
+      name: product.brand.name,
+    },
+  }));
 }
 
 export async function getAllPopularProducts(): Promise<ProductCardProps[]> {
@@ -35,7 +74,7 @@ export async function getAllPopularProducts(): Promise<ProductCardProps[]> {
     rating: 5, 
     imageSrc: product.imageUrl,
     isOnSale: product.onSale,
-    discountAmount: Number(product.discountAmount || 0),
+    discountAmount: Number(product.promoPrice || 0),
     category: {
       id: product.category.id,
       name: product.category.name,
